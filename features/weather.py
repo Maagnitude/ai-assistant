ba87523ce539fa42cd2ff91e5b0126cfe2131f44
Maagnitude <yiwrgos_v.i.p@hotmail.com>
2023-09-14 14:28:13 +0300

    feat: assistant tells the weather

---
+++ features/weather.py
@@ -0,0 +1,25 @@
+import requests
+import sys
+from datetime import datetime
+import json
+
+def get_weather(location, date=datetime.now().strftime("%Y-%m-%d")):
+    api_key = 'HGYLZKAKK25WVYSZVVCFE5LUE'
+    
+    try:
+        # Make the API request
+        response = requests.request("GET", f"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{location.lower()}?unitGroup=metric&include=current&key={api_key}&contentType=json")
+        data = response.json()
+        
+        # Extract weather information from the response
+        temperature = data['days'][0]['temp']
+        conditions = data['days'][0]['conditions']
+        
+        # Convert the information into a spoken response
+        msg = f"Today's weather in {location} is {conditions} with an average temperature of {temperature} degrees Celsius."
+        
+    except Exception as e:
+        print(f"Error fetching weather data: {e}")
+        msg = "Sorry, I couldn't get the weather information at this time."
+    
+    return msg
